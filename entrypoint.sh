69270226de5f9b2aafde8374e237c2084d27a396
Eddy Kim <eddyhkim@gmail.com>
2021-09-30 13:33:17 -0700

    Cleanup entrypoint bash and some comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,15 @@ TARG_DIR="${JEKYLL_DESTINATION:-_site}"
 BRANCH="gh-pages"
 export BUNDLE_BUILD__SASSC=--disable-march-tune-native
 
+if [ -z "$DEST_REPO_GIT" ]; then
+  echo "Missing required variable: DEST_REPO_GIT"
+  exit 1
+fi
+if [ -z "$DEST_REPO_DEPLOY_KEY" ]; then
+  echo "Missing required variable: DEST_REPO_DEPLOY_KEY"
+  exit 1
+fi
+
 echo "Installing gems"
 
 # By placing gems into thsi path, we can cache it in the workflow file with github actions/cache
@@ -29,22 +38,18 @@ cd ${TARG_DIR}
 mkdir -p ~/.ssh/
 echo "$DEST_REPO_DEPLOY_KEY" > ~/.ssh/deploy.key
 
-# using the ruby-2.7.2 docker image, the debian setup seems to ignore  the
-# use specific ~/.ssh/config file.  So this workaround puts the directives
-# into the glboal /etc/ssh/ssh_config instead
+# When using the ruby-2.7.2 docker image, the debian setup seems to ignore the
+# user-specific ~/.ssh/config file.  This workaround puts the directives
+# into the glboal /etc/ssh/ssh_config instead.
 RUN echo "    IdentityFile /github/home/.ssh/deploy.key" >> /etc/ssh/ssh_config
 RUN echo "    StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
-#echo "Host *" >> ~/.ssh/config
-#echo "  IdentityFile ~/.ssh/deploy.key" >> ~/.ssh/config
-#echo "  StrictHostKeyChecking no" >> ~/.ssh/config
-
-# don't need keyscan if we're disabling strict hostkey checking
-# ssh-keyscan github.com 2>&1 >> ~/.ssh/known_hosts
-
+# ssh doesn't like to work with insecure data files.
 chmod 600 ~/.ssh/*
 chmod 700 ~/.ssh
 
+# Tests the ssh key to make sure it's usable, but it won't stop the script if not.
+# Hopefully the ssh output will point to the source of the later failure.
 ssh -i ~/.ssh/deploy.key -T git@github.com || true
 
 git config --global init.defaultBranch master
